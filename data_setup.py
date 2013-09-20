2f129201c91febe074260639a5a0ae1c41e5136c
Søren B. Vrist <s@vrist.dk>
2013-09-20 22:00:55 +0200

    Bugfix

--- data_setup.py
+++ data_setup.py
@@ -1,7 +1,7 @@
 # -*- coding: cp1252 -*-
 # husk r foran hvis der står 'backslash'\ i filnavnet
 #filer = [r'X:\TREASURY\FrontOffice\Cash management\Filer Jacob\transactions 277450550 02-01-2013-15-04-2013 exported 16-04-2013.csv']
-file_dir = 'C:\\Users\\sbv\\Desktop\\transactions'
-#output_filename = r'Q:\Cash flow\277450550.csv'
+file_dir = 'C:\\Users\\sbv\\Desktop\\transactions\\'
+output_filename = 'C:\\Users\\sbv\\Desktop\\transactions\\output.csv'
 AMOUNT_COL = 5
 SKIP_ROWS = 8
--- parse.py
+++ parse.py
@@ -1,16 +1,18 @@
 # -*- coding: cp1252 -*-
 import csv
 import sys
-import os
-from data_setup import *
+from status import dir_to_accounts
+from data_setup import file_dir, AMOUNT_COL, SKIP_ROWS
+
 
 def skip_rows(reader, rows):
     for i in range(rows):
         reader.next()
 
+
 def less_than_zero(col):
     return float(col.replace(',', '.')) < 0
-    
+
 
 def invalid_row(line_num, row):
     invalid = len(row) < AMOUNT_COL
@@ -18,17 +20,13 @@ def invalid_row(line_num, row):
         sys.stderr.write('Line {}. wrong number of cols: {} (< {}): |{}|\n'
                          .format(line_num, len(row), AMOUNT_COL, row))
     return invalid
-        
 
 
-def main(idnumber):
-    with open('%d.csv' % idnumber, 'wb') as output:
+def main(idnumber, files):
+    with open('%s.csv' % idnumber, 'wb') as output:
         w = csv.writer(output, dialect='excel', delimiter=';')
-        wrote_header = False
-        for input_fn in os.listdir(file_dir):
-            input_filename = os.path.join(file_dir, input_fn)
-            if not 'transactions %d'%idnumber in input_filename:
-                continue
+        for f in files:
+            input_filename = f.filename
             print "Reading %s" % input_filename
             with open(input_filename, 'rb') as f:
                 csv_file = csv.reader(f, dialect='excel', delimiter=';')
@@ -37,20 +35,20 @@ def main(idnumber):
                     line_num = ln + SKIP_ROWS + 1
                     if invalid_row(line_num, row):
                         continue
-                    
+
                     amount = row[AMOUNT_COL - 1]
                     try:
                         if less_than_zero(amount):
                             w.writerow(row)
                     except ValueError:
                         sys.stderr.write(
-                            'L{}: Failed to parse \'{}\' as floating number. {}\n'
+                            'L{}: Failed to parse \'{}\' as '
+                            'floating number. {}\n'
                             .format(line_num, amount, row)
                         )
         print "Output in %s" % (output.name)
 
 
 if __name__ == '__main__':
-    for a in accounts:
-        main(a)
-
+    for a, files in dir_to_accounts(file_dir).items():
+        main(a, files)
