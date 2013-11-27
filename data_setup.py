9058aff5234d173d68c34ed90e2fbfafbc5a4172
Søren B. Vrist <s@vrist.dk>
2013-11-27 22:36:13 +0100

    more col defs

--- data_setup.py
+++ data_setup.py
@@ -1,7 +1,11 @@
 # -*- coding: cp1252 -*-
-# husk r foran hvis der står 'backslash'\ i filnavnet
-#filer = [r'X:\TREASURY\FrontOffice\Cash management\Filer Jacob\transactions 277450550 02-01-2013-15-04-2013 exported 16-04-2013.csv']
-file_dir = 'C:\\Users\\sbv\\Desktop\\transactions\\'
-output_filename = 'C:\\Users\\sbv\\Desktop\\transactions\\output.csv'
+# husk r foran hvis der stÃ¥r 'backslash'\ i filnavnet
+file_dir = r'Q:\Cash flow\Raadata\\'
+
+BENEF_COL = 1
+BOOK_COL = 2
+VAL_COL = 3
+DESCR_COL = 4
 AMOUNT_COL = 5
+
 SKIP_ROWS = 8
